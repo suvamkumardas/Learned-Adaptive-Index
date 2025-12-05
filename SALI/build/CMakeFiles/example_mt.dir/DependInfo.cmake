e5cf4eb95aa46b45cb4fa84761307aafb752a82e
suvamkumardas <39821919+suvamkumardas@users.noreply.github.com>
2025-12-04 22:52:38 -0400

    Add files via upload

---
+++ SALI/build/CMakeFiles/example_mt.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/suvam/VS-Code-workspace/Generalized_Learned_Adaptive_Index_EXT_Hash_Model_Retraining/SALI/src/examples/example_multithread.cpp" "CMakeFiles/example_mt.dir/src/examples/example_multithread.cpp.o" "gcc" "CMakeFiles/example_mt.dir/src/examples/example_multithread.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ SALI/src/examples/example_multithread.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <sali.h>
+#include "omp.h"
+
+using namespace std;
+
+int main() {
+  sali::SALI<int, int> sali;
+
+  int key_num = 1000;
+  pair<int, int> *keys = new pair<int, int>[key_num];
+  for (int i = 0; i < 1000; i++) {
+    keys[i]={i,i};
+  }
+  sali.bulk_load(keys, 1000);
+
+  omp_set_num_threads(12);
+
+#pragma omp parallel for schedule(static, 12)
+  for (int i = 1000; i < 2000; i++) {
+    sali.insert(i,i);
+  }
+#pragma omp parallel for schedule(static, 12)
+  for (int i = 0; i < 2000; i++) {
+    int val;
+    sali.at(i, val);
+    std::cout<<"value at "<<i<<": "<<val<<std::endl;
+  }
+
+  return 0;
+}
